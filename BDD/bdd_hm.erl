9c65bbc39ce4dc9cf2e92e80ee1396b0d6e6cc89
Nithin Vadukkumchery Rajendrakumar <vrnithinkumar@gmail.com>
2021-03-01 15:37:53 +0100

    BDD: Adding basic types and terms

--- BDD/bdd_hm.erl
+++ BDD/bdd_hm.erl
@@ -1,2 +1,16 @@
 -module(bdd_hm).
 % convert the js code in erlang
+
+% terms
+var(Name)        -> {var, Name}.
+abs(Name, Body)  -> {abs, Name, Body}.
+app(Left, Right) -> {app, Left, Right}.
+ann(Term, Type)  -> {ann, Term, Type}.
+
+% types
+tVar(Name)           -> {tVar, Name }.
+tForall(Name, Body)  -> {tForall, Name, Body}.
+tFun(Left, Right)    -> {tFun, Left, Right}.
+tApp(Left, Right)    -> {tApp, Left, Right}.
+tMeta(Id, Tvs, Mono) -> {tMeta, Id, Tvs, null, Mono}.
+tSkol(Id)            -> {tSkol, Id}.
